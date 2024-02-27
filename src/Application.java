1db93e6f11a11e0b990f27af763da04659680bd6
Nicrionn <nicholastanjunming23@gmail.com>
2024-02-27 16:06:09 +0800

    .

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,4 @@
 //Compile.bat and Run.bat runs this file
-
-import javax.swing.SwingUtilities;
-
-import ui.MainWindow;
-
 public class Application {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MainWindow window = new MainWindow();
-            window.setVisible(true);
-        });
-    }
+
 }
