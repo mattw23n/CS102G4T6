8a59e80c7a706224a819270b4e400cfac5fe17b1
anlhq <88914442+anlhq@users.noreply.github.com>
2024-03-20 22:40:19 +0800

    Add scoreboard to RoundOnePanel
    unfinished

--- src/GUI/RoundOnePanel.java
+++ src/GUI/RoundOnePanel.java
@@ -34,10 +34,41 @@ public class RoundOnePanel extends JPanel{
         GridConstraints.gridy = 0;
         contentPanel.add(descriptionLabel, GridConstraints);
 
+        JLabel pointLabel = new JLabel("Score");
+        // GridConstraints.weightx = 0.5;
+        GridConstraints.gridx = 10;
+        GridConstraints.gridy = 0;
+        contentPanel.add(pointLabel, GridConstraints);
+
+        JLabel p1PointsLabel = new JLabel("Player 1: ");
+        // GridConstraints.weightx = 0.5;
+        GridConstraints.gridx = 10;
+        GridConstraints.gridy = 4;
+        contentPanel.add(p1PointsLabel, GridConstraints);
+
+        JLabel p2PointsLabel = new JLabel("Player 2: ");
+        // GridConstraints.weightx = 0.5;
+        GridConstraints.gridx = 10;
+        GridConstraints.gridy = 8;
+        contentPanel.add(p2PointsLabel, GridConstraints);
+
+        JLabel p3PointsLabel = new JLabel("Player 3: ");
+        // GridConstraints.weightx = 0.5;
+        GridConstraints.gridx = 10;
+        GridConstraints.gridy = 12;
+        contentPanel.add(p3PointsLabel, GridConstraints);
+
+        JLabel p4PointsLabel = new JLabel("Player 4: ");
+        // GridConstraints.weightx = 0.5;
+        // GridConstraints.weighty = 0.1;
+        GridConstraints.gridx = 10;
+        GridConstraints.gridy = 18;
+        contentPanel.add(p4PointsLabel, GridConstraints);
+
         nextButton = new JButton("Next");
-        GridConstraints.weightx = 0.5;
+        // GridConstraints.weightx = 0.5;
         GridConstraints.gridx = 0;
-        GridConstraints.gridy = 10;
+        GridConstraints.gridy = 8;
         contentPanel.add(nextButton, GridConstraints);
         
         //Listener for "Next" Button
@@ -57,8 +88,6 @@ public class RoundOnePanel extends JPanel{
 
         // Create and display multiple images
         JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Example grid layout with 2x2 images
-        GridConstraints.gridx = 0;
-        GridConstraints.gridy = 40;
 
         //Testing Multiple Images
         for (int i = 1; i <= 4; i++) {
@@ -72,6 +101,8 @@ public class RoundOnePanel extends JPanel{
             // Add the image label to the panel
             imagePanel.add(imageLabel);
         }
+        GridConstraints.gridx = 0;
+        GridConstraints.gridy = 500;
         contentPanel.add(imagePanel, GridConstraints);
     }
     private static void setImage (JLabel label, String imagePath){
